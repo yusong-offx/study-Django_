3a2c9f142e9ac3837e22a825ce50a8b34f33fed7
yusong-offx <42.4.yusong@gmail.com>
2021-11-22 00:14:40 +0900

    Done

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+#from polls import views
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	#path('polls/', include('polls.urls')),
+	# path('polls/', views.index, name='index'),
+	# path('polls/<int:question_id>/', views.detail, name='detail'),
+	# path('polls/<int:question_id/results/', views.results, name='results'),
+	# path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+	path('polls/', include('polls.urls')),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from polls.models import Question, Choice
 
 # Create your views here.
+
+def index(request):
+	latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+	print(latest_question_list)
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question':question})
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html',{
+			'question': question,
+			'error_message': "You didn't select a choice",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+	print("redirect check")
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question':question})
