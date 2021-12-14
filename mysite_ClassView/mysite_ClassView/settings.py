b0a4b6c01c6e986c0adf36b6c82179d75ea44634
yusong-offx <42.4.yusong@gmail.com>
2021-12-14 18:35:05 +0900

    try classview done

--- mysite_ClassView/mysite_ClassView/settings.py
+++ mysite_ClassView/mysite_ClassView/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-	'books.apps.BooksConfig',
+    'books.apps.BooksConfig',
 ]
 
 MIDDLEWARE = [
